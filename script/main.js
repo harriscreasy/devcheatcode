b91e78e934e85eba3b95c703836275daf2117cf3
harriscreasy <harrisbzaoui@gmail.com>
2021-04-07 16:52:16 +0200

    Nouvelle version

---
+++ script/main.js
@@ -0,0 +1,8 @@
+// gsap.to(".carda", {duration: 1, delay: 0.5, opacity: 1});
+gsap.to(".presentation", {duration: 1, delay: 0.5, opacity: 1, stagger: 0.25});
+// gsap.from(".carda", {duration: 2, delay: 1, opacity: 0, stagger: 0.25});
+// gsap.to(".secondsection", {background: "green", duration: 2, delay: 1, stagger: 0.25});
+// gsap.to(".pcla", {color: "red", duration: 2, delay: 1, stagger: 0.25});
+
+
+
